46c5581448109aee0f71bba2a41a07c38f0800c1
agxmohamed <lucas@magiosteam.com>
2015-12-10 15:16:27 -0300

    Actualizando las vistas

--- Flask/Task/models/category.py
+++ Flask/Task/models/category.py
@@ -6,6 +6,6 @@ class Category(db.Model):
     """ Comments - tabla de comentarios de las tareas """
 
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text)
+    content = db.Column(db.String)
     date = db.Column(db.DateTime, default=datetime.now, index=True)
     task = db.Column(db.Integer, ForeignKey('task.id'))
--- Flask/webclient/app/views.py
+++ Flask/webclient/app/views.py
@@ -1,20 +1,40 @@
-from flask import request, redirect, render_template, url_for
+import requests
+from flask import request, redirect, render_template, url_for, json
 from app import app
 
 @app.route('/')
-@app.route('/login')
-def index():
-	return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+	if request.method == 'POST':
+		_username = request.form['username']
+		_password = request.form['password']
+
+		url = 'http://127.0.0.1/api/user'
+		q = {'filters': [{'name': 'username', 'op': 'eq', 'val': _username}, {'name': 'password', 'op': 'eq', 'val': _password}]}
+		response = requests.get(url, params={'q': json.dumps(q)})
+		r = response.json()
+		if r['num_results'] == 0:
+			return render_template('login.html')
+		else:
+			return redirect("/manager", code=302)
+	else:
+		return render_template('login.html')
+
 @app.route('/manager')
 def manager():
-	id = request.args.get('id')
-	id = int(id)
 	active1="class=active"
 	active2="class=active"
 	active3="class=active"
-
+	
+	id = request.args.get('id')
+	id = int(id)
+	
 	if id == 1:
-		return render_template('pendingtask.html', active1=active1)
+		url = 'http://127.0.0.1/api/task'
+		result = requests.get(url)
+		response = json.loads(result.text)		
+		return render_template('pendingtask.html', active1=active1, response=response)
 	elif id == 2:
 		return render_template('completetask.html', active2=active2)
 	elif id == 3:

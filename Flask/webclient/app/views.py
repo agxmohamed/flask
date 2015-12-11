bb86e39c507ca01c0a3cd684aa86fc665420325b
agxmohamed <lucas@magiosteam.com>
2015-12-11 17:32:37 -0300

    ASD

--- Flask/webclient/app/views.py
+++ Flask/webclient/app/views.py
@@ -1,32 +1,34 @@
+from werkzeug.routing import BaseConverter
 import requests
 from flask import request, redirect, render_template, url_for, json
 from app import app
 
 @app.route('/')
-
 @app.route('/login', methods=['POST'])
 def login():
 	if request.method == 'POST':
 		_username = request.form['username']
 		_password = request.form['password']
-
+		_session = None
 		url = 'http://127.0.0.1/api/user'
 		q = {'filters': [{'name': 'username', 'op': 'eq', 'val': _username}, {'name': 'password', 'op': 'eq', 'val': _password}]}
+		print q
 		response = requests.get(url, params={'q': json.dumps(q)})
 		r = response.json()
+		_session = r['objects'][0]['id']
 		if r['num_results'] == 0:
 			return render_template('login.html')
 		else:
-			return redirect("/manager", code=302)
+			return redirect("/manager/" + str(_session) , code=302)
 	else:
 		return render_template('login.html')
 
-@app.route('/manager')
-def manager():
+@app.route("/manager/<int:params>")
+def manager(params):
 	active1="class=active"
 	active2="class=active"
 	active3="class=active"
-	
+
 	if request.args.get('id') == None:
 		id = 1
 	else:
@@ -36,7 +38,8 @@ def manager():
 		url = 'http://127.0.0.1/api/task'
 		result = requests.get(url)
 		response = json.loads(result.text)		
-		return render_template('pendingtask.html', active1=active1, response=response)
+		print params
+		return render_template('pendingtask.html', active1=active1, response=response, user_id=params)
 	elif id == 2:
 		return render_template('completetask.html', active2=active2)
 	elif id == 3:

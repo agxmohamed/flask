1b1e6b07bae5e24c4cbefc17dce74a51c75d699f
agxmohamed <lucas@magiosteam.com>
2015-12-10 21:40:43 -0300

    update

--- Flask/Task/models/task.py
+++ Flask/Task/models/task.py
@@ -7,13 +7,18 @@ class Task(db.Model):
     a realizar """
 
     id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.Integer, ForeignKey('status.id'), default=1)
-    priority = db.Column(db.Integer, ForeignKey('priority.id'), default=1)
-    category = db.Column(db.Integer, ForeignKey('category.id'))
-    comment = db.Column(db.Integer, ForeignKey('comment.id'))
-    user = db.Column(db.Integer, ForeignKey('user.id'))
+    status_id = db.Column(db.Integer, ForeignKey('status.id'), default=1)
+    priority_id = db.Column(db.Integer, ForeignKey('priority.id'), default=1)
+    category_id = db.Column(db.Integer, ForeignKey('category.id'))
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
     title = db.Column(db.Unicode)
     description = db.Column(db.Text)
     expiry_date = db.Column(db.DateTime)
     creation_date = db.Column(db.DateTime, onupdate=datetime.now)
     last_connection = db.Column(db.DateTime, default=datetime.now)
+
+    status = db.relationship('Status')
+    priority = db.relationship('Priority')
+    category = db.relationship('Category')
+    comment = db.relationship('Comment')
+    user = db.relationship('User')
